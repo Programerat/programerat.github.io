a12ef1791be04a48da57f421c5a605d473d99316
Diar <diar@billie.io>
2021-11-21 17:45:18 +0100

    adding table of contents

---
+++ assets/js/table_of_contents.js
@@ -0,0 +1,14 @@
+var article = document.getElementsByClassName('post')[0];
+var headers = article.getElementsByTagName('h3');
+
+function appendHeader(header){
+  var content = document.getElementsByClassName('table_of_contents');
+  var html = '<li><a href="#'+header+'">'+header+'</a></li>';
+  content[0].innerHTML += html;
+}
+
+for(var i=0; i < headers.length; i++) {
+  appendHeader(headers[i].innerHTML);  
+  headers[i].setAttribute('id', headers[i].innerHTML)
+}
+
