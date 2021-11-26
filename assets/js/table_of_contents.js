5fe758a5e9ecabbbbb1ac04312a7ca4eb9f12b50
Diar <diar@billie.io>
2021-11-26 18:41:13 +0100

    fix js

--- assets/js/table_of_contents.js
+++ assets/js/table_of_contents.js
@@ -1,14 +1,18 @@
-var article = document.getElementsByClassName('post')[0];
-var headers = article.getElementsByTagName('h3');
+var article = getElementByClass('post');
 
-function appendHeader(header){
-  var content = document.getElementsByClassName('table_of_contents');
-  var html = '<li><a href="#'+header+'">'+header+'</a></li>';
-  content[0].innerHTML += html;
-}
+if (article != null) {
 
-for(var i=0; i < headers.length; i++) {
-  appendHeader(headers[i].innerHTML);  
-  headers[i].setAttribute('id', headers[i].innerHTML)
+  var headers = article.getElementsByTagName('h3');
+
+  function appendHeader(header){
+    var content = document.getElementsByClassName('table_of_contents');
+    var html = '<li><a href="#'+header+'">'+header+'</a></li>';
+    content[0].innerHTML += html;
+  }
+
+  for(var i=0; i < headers.length; i++) {
+    appendHeader(headers[i].innerHTML);  
+    headers[i].setAttribute('id', headers[i].innerHTML)
+  }
 }
 
