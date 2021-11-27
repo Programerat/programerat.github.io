eda7c90c2c5acb1e99badd30c1d4cac0b94d029f
Diar <diar@billie.io>
2021-11-27 14:42:43 +0100

    fix

--- assets/js/table_of_contents.js
+++ assets/js/table_of_contents.js
@@ -9,6 +9,14 @@ if (article != null) {
     tocHeaders.push(headers[i].innerText);
   }
 
-  appendElementsToElement(getElementByClass('toc-content'), tocHeaders, 'li', false); // rethink this
+  appendListToList(getElementByClass('toc-content'), tocHeaders);
 }
 
+function appendListToList(list, items) {
+  for (var i = 0; i < items.length; i++) {
+    var item = items[i];
+    var newElement = document.createElement('li');
+    newElement.innerHTML = '<a href="#' + item + '">' + item + '</a>';
+    list.appendChild(newElement);
+  }
+}
