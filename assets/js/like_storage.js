4836a40d6892d4fc41680c654b2fc109b0e2fd46
Did <diarselimi92@gmail.com>
2022-07-24 19:29:25 +0200

    Like functionality

---
+++ assets/js/like_storage.js
@@ -0,0 +1,57 @@
+var myHeaders = new Headers();
+let totalLikes = 0;
+myHeaders.append("Content-Type", "application/json");
+
+var raw = "";
+
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+
+function updateLikeNumbers(likes) {
+    let element = document.getElementById('like_counter');
+    element.text = likes;
+    totalLikes = likes;
+}
+
+function sendNewLike() {
+    totalLikes++;
+    let raw = JSON.stringify({
+        "counter": totalLikes
+    });
+
+    var requestOptions = {
+        method: 'PUT',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+      };
+
+    window.fetch("https://getpantry.cloud/apiv1/pantry/15efcf15-d11d-4d34-9f7f-f0bd2c8a8680/basket/1", requestOptions)
+      .then(response => parseResponseData(response))
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+}
+
+async function parseResponseData(response) {
+    let data = await response.text();
+    let jsonData = JSON.parse(data);
+
+    updateLikeNumbers(jsonData.counter ?? 0);
+}
+
+async function fetchText() {
+    let response = await fetch('https://getpantry.cloud/apiv1/pantry/15efcf15-d11d-4d34-9f7f-f0bd2c8a8680/basket/1');
+
+    console.log(response.status); // 200
+    console.log(response.statusText); // OK
+
+    if (response.status === 200) {
+       parseResponseData(response);
+    }
+}
+ 
+fetchText();
